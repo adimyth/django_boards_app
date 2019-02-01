b424bdc395854393a51fe3c88ed0213b4c70fdc0
adimyth <mishraaditya6991@gmail.com>
2019-02-01 13:19:14 +0530

    modifies settings.py

--- board_project/settings.py
+++ board_project/settings.py
@@ -152,6 +152,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_FILE_PATH = 'tmp/email-messages/'
 EMAIL_HOST = 'smtp.gmail.com'   # mail service smtp
 EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 EMAIL_USE_TLS = True
 
 
