fa44c83eb4690392a569dfb6a048d578d6cd7629
adimyth <mishraaditya6991@gmail.com>
2019-02-04 12:45:55 +0530

    modifies models.py to follow best practices

--- board_app/models.py
+++ board_app/models.py
@@ -5,10 +5,19 @@ from markdown import markdown
 from django.utils.text import slugify
 
 
+# 1. fields of the model
+# 2. Meta
+# 3. def __str__
+# 4. other special methods
+# 5. save
+# 6. other methods
 class Board(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=100)
     slug = models.SlugField(null=True)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
@@ -33,38 +42,34 @@ class Board(models.Model):
         board = Board.objects.get(name=self)
         try:
             return Post.objects.filter(topic__board=board).order_by('created_at').last().created_by
-        except Exception:
-            pass
+        except Exception as e:
+            print(f"Exception: {e}")
 
 
 class Topic(models.Model):
-
-    class Meta:
-        ordering = ['-views']
-
     subject = models.CharField(max_length=100)
     last_updated = models.DateTimeField(auto_now_add=True)
-    views = models.IntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
     board = models.ForeignKey(Board, related_name='topics', on_delete='cascade')
     created_by = models.ForeignKey(User, related_name='topics', on_delete='cascade')
     slug = models.SlugField(null=True)
 
+    class Meta:
+        ordering = ['-views']
+
     def __str__(self):
         return self.subject
-
-    def get_posts_count(self):
-        topic = Topic.objects.get(subject=self)
-        return topic.posts.count()
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self)
         super(Topic, self).save(*args, **kwargs)
 
+    def get_posts_count(self):
+        topic = Topic.objects.get(subject=self)
+        return topic.posts.count()
+
 
 class Post(models.Model):
-    class Meta:
-        ordering = ['-created_at']
-
     message = models.TextField(max_length=100)
     topic = models.ForeignKey(Topic, related_name='posts', on_delete='cascade')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -72,8 +77,8 @@ class Post(models.Model):
     created_by = models.ForeignKey(User, related_name='posts', on_delete='cascade')
     updated_by = models.ForeignKey(User, null=True, on_delete='cascade')
 
+    class Meta:
+        ordering = ['-created_at']
+
     def get_message_as_markdown(self):
         return mark_safe(markdown(self.message, safe_mode='escape'))
-
-    # def __str__(self):
-    #     return self.message
